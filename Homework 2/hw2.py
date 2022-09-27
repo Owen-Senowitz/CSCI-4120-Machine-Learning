c074bb0c95268fe685d557e530628120ca6ed9af
Owen Senowitz <owen.senowitz@gmail.com>
2022-09-27 14:50:58 -0400

    added functions and confusion matrix

--- Homework 2/hw2.py
+++ Homework 2/hw2.py
@@ -1,24 +1,36 @@
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.svm import SVC
 from sklearn.datasets import make_blobs
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
 from yellowbrick.cluster import KElbowVisualizer
-
-#from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
 
 X, y_true = make_blobs(n_samples=300, centers=4,
                        cluster_std=0.60, random_state=0)
 
-plt.scatter(X[:, 0], X[:, 1], s = 30, color ='b')
-plt.xlabel('X')
-plt.ylabel('Y')
-#uncomment to show scatter plot
-#plt.show()
-plt.clf()
+def scatterPlot():
+    plt.scatter(X[:, 0], X[:, 1], s = 30, color ='b')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+    plt.clf()
 
-visualizer = KElbowVisualizer(KMeans(), k=(1,10))
-visualizer.fit(X)
-visualizer.show()
+def elbow():
+    visualizer = KElbowVisualizer(KMeans(), k=(1,10))
+    visualizer.fit(X)
+    visualizer.show()
 
-# TODO determine the best k for k-means
-# TODO calculate accuracy for best K
-# TODO draw a confusion matrix
+def confusionMatrix():
+    X_train, X_test, y_train, y_test = train_test_split(X, y_true, random_state=0)
+    clf = SVC(random_state=0)
+    clf.fit(X_train, y_train)
+    SVC(random_state=0)
+    plot_confusion_matrix(clf, X_test, y_test)  
+    plt.show()
+    plt.clf()
+
+#uncomment to run functions
+scatterPlot()
+elbow()
+confusionMatrix()
