75da44bd10a4a87e2001218548b19d9f9f473954
Owen Senowitz <owen.senowitz@gmail.com>
2022-09-27 18:35:17 -0400

    scatter plot updated

--- Homework 2/hw2.py
+++ Homework 2/hw2.py
@@ -10,7 +10,7 @@ X, y_true = make_blobs(n_samples=300, centers=4,
                        cluster_std=0.60, random_state=0)
 
 def scatterPlot():
-    plt.scatter(X[:, 0], X[:, 1], s = 30, color ='b')
+    plt.scatter(X[:, 0], X[:, 1], c=y_true, s=20, cmap='plasma')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
